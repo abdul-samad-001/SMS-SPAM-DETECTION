9567d414cbf24ae4f7fbc839bc3242ae0c16d7f4
abdul-samad-001 <abdulsamad9040474@gmail.com>
2026-01-25 19:01:47 +0530

    Add CORS support to Flask application

--- api/index.py
+++ api/index.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 import pickle
 
 app = Flask(__name__, template_folder="templates")
+CORS(app)
 
 model = pickle.load(open("model.pkl", "rb"))
 vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
